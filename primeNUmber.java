c8cd93e842835790f726a7182d8d095d4bd894da
Ayush mane <114165081+ayushmane77@users.noreply.github.com>
2024-06-08 19:02:33 +0530

    Update primeNUmber.java

--- primeNUmber.java
+++ primeNUmber.java
@@ -27,6 +27,27 @@ public class primeNUmber {
 
 
     // better approach 
+
+//     class Solution {
+//     public boolean isPrime(int N) {
+//       // code here
+//       if(N==1){
+//           return false;
+//       }
+//       if(N==2|| N==3){
+//           return true;
+//       }
+//       if(N%2==0 || N%3==0){
+//           return false;
+//       }
+//       for(int i=5;i*i<=N;i=i+6){
+//           if(N%i==0 || N%(i+2)==0){
+//               return false;
+//           }
+//       }
+//       return true;
+//     }
+// }
     
     public static boolean checkPrime(int n){
         if(n==1){
