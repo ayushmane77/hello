30c6e974d71011ccb27ab7390d9055386a71f530
ayushmane77 <ayushmane145@gmail.com>
2024-08-16 19:11:11 +0530

    commit

---
+++ myName/arrays/LongestEvenOddSubArray.java
@@ -0,0 +1,49 @@
+package myName.arrays;
+import java.util.Scanner;
+public class LongestEvenOddSubArray {
+
+    // NAIVE APPROACH
+
+    // public static int lengthOfMaximumEvenOddSubarray(int arr[]){
+    //     int length=1;
+    //     for(int i=0;i<arr.length;i++){
+    //         int currentElement=1;
+    //         for(int j=i+1;j<arr.length;j++){
+    //             if((arr[j]%2==0 && arr[j-1]!=0) || (arr[j]%2!=0 && arr[j-1]%2==0)){
+    //                 currentElement++;
+    //             }
+    //         }
+    //         length=Math.max(length,currentElement);
+    //     }
+    //     return length;
+
+    // BETTER APPROACH
+    
+    public static int lengthOfMaximumEvenOddSubarray(int arr[]){
+        int length=1;
+        int maxlength=1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]%2==0 && arr[i-1]%2!=0 || arr[i]%2!=0 && arr[i-1]%2==0){
+                length++;
+            }  // [5,10,20,6,3,8]    
+            else{
+                length=1;
+            }
+            maxlength=Math.max(maxlength, length);
+        }
+        return maxlength;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("enter the elements inside array");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int length=lengthOfMaximumEvenOddSubarray(arr);
+        System.out.println("the length of maximum longest evenodd subarray is " + length);
+        sc.close();
+    }
+}
--- myName/arrays/MaximumSubArraySum.java
+++ myName/arrays/MaximumSubArraySum.java
@@ -2,18 +2,33 @@ package myName.arrays;
 import java.util.*;
 public class MaximumSubArraySum {
 
-    public static int maximumSubArraySum(int arr[]){
-        int maxSum=Integer.MIN_VALUE;
+    // NAIVE APPRAOCH
+
+    // public static int maximumSubArraySum(int arr[]){
+    //     int maxSum=Integer.MIN_VALUE;
         
-        for(int i=0;i<arr.length;i++){
-            int currentSum=0;
-            for(int j=i;j<arr.length;j++){ // [5,4,2,6,7]
-                currentSum=currentSum+arr[j];
-                maxSum=Math.max(maxSum, currentSum);
-            }
+    //     for(int i=0;i<arr.length;i++){
+    //         int currentSum=0;
+    //         for(int j=i;j<arr.length;j++){ // [5,4,2,6,7]
+    //             currentSum=currentSum+arr[j];
+    //             maxSum=Math.max(maxSum, currentSum);
+    //         }
+    //     }
+    //     return maxSum;
+    // }
+
+    // BETTER APPROCH
+
+    public static int maximumSubArraySum(int arr[]){
+        int maxSum=arr[0];
+        int subArraySum=arr[0];
+        for(int i=1;i<arr.length;i++){
+            subArraySum=Math.max(subArraySum + arr[i], arr[i]);
+            maxSum=Math.max(maxSum, subArraySum);
         }
         return maxSum;
     }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the size of array");
