419237b7e1da6752a95562500b1f9a0157d01548
ayushmane77 <ayushmane145@gmail.com>
2025-01-03 23:32:57 +0530

    commit

--- myName/sorting/InsertionSort.java
+++ myName/sorting/InsertionSort.java
@@ -2,16 +2,16 @@ package myName.sorting;
 import java.util.Scanner;
 public class InsertionSort {
     public static int[] SortingByInsertionSort(int arr[]){
-        for(int i=1;i<arr.length;i++){
-            int key=arr[i];
-            int j=i-1;
-            while(j>=0 && arr[j]>key){
-                arr[j+1]=arr[j];
-                j--;
+        for(int i=1;i<arr.length;i++){  // starting from 2nd element
+            int key=arr[i]; // key is the element to be compared
+            int j=i-1;      // j is the index of element just before key
+            while(j>=0 && arr[j]>key){ // if element of arr is greater than key
+                arr[j+1]=arr[j]; // shifting the element to right
+                j--; // moving to left
             }
-            arr[j+1]=key;
+            arr[j+1]=key;   // inserting the key at correct position
         }
-        return arr;
+        return arr; // returning the sorted array
     }
     // Time complexity of Insertion Sort is O(n^2) in general, but O(n) in best case(when array is already sorted).
     // Space complexity of Insertion Sort is O(1).
