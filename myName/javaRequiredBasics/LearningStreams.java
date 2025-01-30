dcf51f5c34a69e15427cd41a0c0b98c3a6737f15
ayushmane77 <ayushmane145@gmail.com>
2025-01-30 11:53:26 +0530

    stream

--- myName/javaRequiredBasics/LearningStreams.java
+++ myName/javaRequiredBasics/LearningStreams.java
@@ -36,8 +36,16 @@ public class LearningStreams {
 
         // ways of creating a stream.
         String names[]={"Ayush","Tushar","Saket","Anish"};
-        Stream<String> stream=Arrays.stream(names);
+        // converting the string array into a list of strings with only elements starting with the character 'A'.
+        List<String> stream=Arrays.stream(names).filter(e->e.startsWith("A")).collect(Collectors.toList());
         System.out.println(stream);
+
+
+        // converting the string array into a list of strings with all elements in lowercase.
+        List<String> myStream=Arrays.stream(names).map(e->e.toLowerCase()).collect(Collectors.toList()); 
+        System.out.println(myStream);
+
+        
 
         Stream<Integer> values=Stream.of(23,52,13,53,64,61,35);
         System.out.println(values);
@@ -45,6 +53,9 @@ public class LearningStreams {
         // Stream<Character> chr=Stream.of('a','w');
         // or
         
+        List<Integer> numbers=Arrays.asList(45,42,67,11,98,23,51,34,27);
+        List<Integer> evenSquaredSum=numbers.stream().filter(e->e%2==0).map(e->e*e).collect(Collectors.toList());
+        System.out.println(evenSquaredSum);
         
         sc.close(); 
     }
