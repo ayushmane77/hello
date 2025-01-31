f37eea152a0140abbdebb9b8dc89a119c41e49ac
ayushmane77 <ayushmane145@gmail.com>
2025-01-31 19:07:55 +0530

    multi threading

--- myName/javaRequiredBasics/MultiThreading/myclass.java
+++ myName/javaRequiredBasics/MultiThreading/myclass.java
@@ -23,6 +23,7 @@ class myThread extends Thread{ // extending the thread class
 }
 
 class newThread extends Thread{
+    @Override
     public void run(){
         for(int i=0;i<=5;i++){
             System.out.println("Thread : Hello");
