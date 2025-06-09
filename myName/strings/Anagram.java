9beaf0c798c698b0d29068ec7dc578dac67ac7bc
ayushmane77 <ayushmane145@gmail.com>
2025-06-10 00:51:24 +0530

    check if two strings are anagram

--- myName/strings/Anagram.java
+++ myName/strings/Anagram.java
@@ -1,24 +1,47 @@
 package myName.strings;
-import java.util.Arrays;
+// import java.util.Arrays;
 import java.util.Scanner;
 public class Anagram {
 
-    public static boolean isAnagram(String str,String str2){
-        if(str.length()!=str2.length()){
+    // naive solution
+
+    // public static boolean isAnagram(String str,String str2){
+    //     if(str.length()!=str2.length()){
+    //         return false;
+    //     }
+    //     char arr[]=str.toCharArray();
+    //     char arr2[]=str2.toCharArray();
+    //     Arrays.sort(arr);
+    //     Arrays.sort(arr2);
+    //     for(int i=0;i<arr.length;i++){
+    //         if(arr[i]!=arr2[i]){
+    //             return false;
+    //         }
+    //     }
+
+    //     return true;
+    // }
+    // time complexity : O(nlogn)
+    // auxiliary space : O(n)
+
+    public static boolean isAnagram(String str1,String str2){
+        int arr[] = new int[256];
+        if(str1.length()!=str2.length()){
             return false;
         }
-        char arr[]=str.toCharArray();
-        char arr2[]=str2.toCharArray();
-        Arrays.sort(arr);
-        Arrays.sort(arr2);
+        for(int i=0;i<str1.length();i++){
+            arr[str1.charAt(i)]++;
+            arr[str2.charAt(i)]--;
+        }
         for(int i=0;i<arr.length;i++){
-            if(arr[i]!=arr2[i]){
+            if(arr[i]!=0){
                 return false;
             }
         }
-
         return true;
     }
+    // time complexity : O(N)
+    // Auxiliary space : O(1)
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter first string");
