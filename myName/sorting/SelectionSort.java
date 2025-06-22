1c7997704e9a8849e5d72c01a23f38341fe1f9a3
ayushmane77 <ayushmane145@gmail.com>
2025-06-22 15:28:30 +0530

    selection sort algorithm

--- myName/sorting/SelectionSort.java
+++ myName/sorting/SelectionSort.java
@@ -1,40 +1,39 @@
 package myName.sorting;
 import java.util.Scanner;
 public class SelectionSort {
-    public static int[] SortingBySelectionSort(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            // int min=arr[i];
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[j]<arr[i]){ 
-                    int temp=arr[j]; 
-                    arr[j]=arr[i]; 
-                    arr[i]=temp;   // [2,5,7,8,9]
-                }
-            }
-        }
-        return arr;
-    }
+    // public static int[] SortingBySelectionSort(int arr[]){
+    //     for(int i=0;i<arr.length;i++){
+    //         // int min=arr[i];
+    //         for(int j=i+1;j<arr.length;j++){
+    //             if(arr[j]<arr[i]){ 
+    //                 int temp=arr[j]; 
+    //                 arr[j]=arr[i]; 
+    //                 arr[i]=temp;   // [2,5,7,8,9]
+    //             }
+    //         }
+    //     }
+    //     return arr;
+    // }
     // Time complexity of Selection Sort is O(n^2) in all cases.
     // Space complexity of Selection Sort is O(1).
     // Selection Sort is not stable.
 
     // OR
-    // public static int[] SortingBySelectionSort(int arr[]){
-    //     for(int i=0;i<arr.length;i++){
-    //         int min=arr[i];
-    //         int minIndex=i;
-    //         for(int j=i+1;j<arr.length;j++){
-    //             if(arr[j]<min){
-    //                 min=arr[j];
-    //                 minIndex=j;
-    //             }
-    //         }
-    //         int temp=arr[i];
-    //         arr[i]=arr[minIndex];
-    //         arr[minIndex]=temp;
-    //     }
-    //     return arr;
-    // }
+    public static int[] SortingBySelectionSort(int arr[]){  // [34,12,55,65,23]
+        for(int i=0;i<arr.length;i++){
+            int min = i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){       // Store the minimum index seen so far and compare it with rest elements 
+                    min = j;
+                }
+            }
+            int temp = arr[i];  // swap the current ith element with the element of minimum index.
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+        return arr;
+    }
+    // Time complexity : O(N^2)
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the size of the array");
