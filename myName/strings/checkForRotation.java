2b9c1592a11fddcefe1b6f641464713054422b86
ayushmane77 <ayushmane145@gmail.com>
2025-07-02 15:40:52 +0530

    check for rotation optimized

--- myName/strings/checkForRotation.java
+++ myName/strings/checkForRotation.java
@@ -2,32 +2,42 @@ package myName.strings;
 import java.util.Scanner;
 public class checkForRotation {
 
-    public static String rotateByOne(String str){
-        char arr[] = str.toCharArray();
-        char temp = arr[0];
-        for(int i=1;i<str.length();i++){
-            arr[i-1] = arr[i];
-        }
-        arr[arr.length-1] = temp;
-        String newStr = new String(arr);
-        return newStr;
-    }
+    // public static String rotateByOne(String str){
+    //     char arr[] = str.toCharArray();
+    //     char temp = arr[0];
+    //     for(int i=1;i<str.length();i++){
+    //         arr[i-1] = arr[i];
+    //     }
+    //     arr[arr.length-1] = temp;
+    //     String newStr = new String(arr);
+    //     return newStr;
+    // }
 
-    public static boolean isRotated(String str, String rts){
-        if(str.equals(rts)){
-            return true;
-        }
-        String rotated=str;
-        for(int i = 0;i<str.length();i++){
-            rotated = rotateByOne(rotated);
-            if(rotated.equals(rts)){
-                return true;
-            }
-        }
-        return false;
-    }
+    // public static boolean isRotated(String str, String rts){
+    //     if(str.equals(rts)){
+    //         return true;
+    //     }
+    //     String rotated=str;
+    //     for(int i = 0;i<str.length();i++){
+    //         rotated = rotateByOne(rotated);
+    //         if(rotated.equals(rts)){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
     // Time complexity : O(N^2)
     // Auxiliary space : O(N)
+
+    // Optimized approach
+
+    public static boolean isRotated(String str, String rts){
+        if(str.length()!=rts.length()){
+            return false;
+        }
+        return ((str + str ).indexOf(rts)>=0 ) ? true: false;
+    }
+    // Time complexity : O(1)
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the string");
