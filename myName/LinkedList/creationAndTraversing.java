9a3b375dc3409b02bc6758a378a472fe45eee27f
ayushmane77 <ayushmane145@gmail.com>
2025-07-17 20:21:03 +0530

    creating and traversing linkedList

--- myName/LinkedList/creationAndTraversing.java
+++ myName/LinkedList/creationAndTraversing.java
@@ -1,17 +1,17 @@
 package myName.LinkedList;
 
-class Node{
+class newNode{
     int data; // data field to store the value
-    Node next; // next field to store the address of the next node
-    Node(int data){ // constructor to initialize the node
+    newNode next; // next field to store the address of the next node
+    newNode(int data){ // constructor to initialize the node
         this.data = data; // assign the data to the node
         this.next = null; // initialize next to null
     }
 }
 public class creationAndTraversing {
 
-    public static void print(Node head){ // method to traverse and print the linked list
-        Node curr = head; // start from the head of the linked list
+    public static void print(newNode head){ // method to traverse and print the linked list
+        newNode curr = head; // start from the head of the linked list
         while(curr!=null){ // loop until we reach the end of the linked list
             System.out.print(curr.data + " "); // print the data of the current node
             curr = curr.next; // move to the next node
@@ -20,9 +20,9 @@ public class creationAndTraversing {
     // Time complexity : O(N)
     // Auxiliary space : O(1)
     public static void main(String[] args) {
-        Node head = new Node(23); // head is actually an object which is having an address.
-        Node temp2 = new Node(45); // temp2 is a reference variable which is pointing to an object.
-        Node temp3 = new Node(63); // temp3 is a reference variable which is pointing to an object.
+        newNode head = new newNode(23); // head is actually an object which is having an address.
+        newNode temp2 = new newNode(45); // temp2 is a reference variable which is pointing to an object.
+        newNode temp3 = new newNode(63); // temp3 is a reference variable which is pointing to an object.
         head.next = temp2; // head is pointing to temp2.
         temp2.next = temp3; // temp2 is pointing to temp3.
         print(head);
